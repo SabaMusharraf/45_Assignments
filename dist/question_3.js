fe8276fb02b8bf9bf87dc69775c1c22426847f07
sabamusharraf <sabahussain337@gmail.com>
2024-03-18 23:27:56 +0500

    Add some more exercise

---
+++ dist/question_3.js
@@ -0,0 +1,8 @@
+"use strict";
+/* 3. Name Cases: Store a person’s name in a variable, and then print that person’s
+name in lowercase, uppercase, and titlecase.*/
+let myName = "Saba Hussain";
+console.log(`In Lower Case`);
+console.log(myName.toLowerCase());
+console.log(`In Upper Case`);
+console.log(`In Upper case`, myName.toUpperCase());
